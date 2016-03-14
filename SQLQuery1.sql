5e780e818c2639d37858184a781231de2401ef87
Douglas Bollella <douglas.bollella@live.com>
2016-03-14 02:16:04 -0300

    inclusao de objetos

---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿SELECT * FROM tblUsuario
+UPDATE tblUsuario SET Usuario = 'douglas' WHERE UsuarioID = 1
---
+++ dbo.tblUsuario.data.sql
@@ -0,0 +1,3 @@
+﻿
+INSERT INTO [dbo].[tblUsuario] ( [Usuario], [Senha], [Administrador]) VALUES ( N'usuario', N'123456', 1)
+
