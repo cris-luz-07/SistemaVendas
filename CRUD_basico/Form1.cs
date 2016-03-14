bbe8feb62776c01bc09ff78c0e748b24b981bf65
Douglas Bollella <douglas.bollella@live.com>
2016-03-13 23:33:35 -0300

    Projeto de Vendas

---
+++ CRUD_basico/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_basico
+{
+    public partial class frmCadastroCliente : Form
+    {
+        public frmCadastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBairro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mskCEP_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void lblUF_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
