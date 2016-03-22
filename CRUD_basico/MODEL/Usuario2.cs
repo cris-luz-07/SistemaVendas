f3943f2cc46446fa4038c782b647b5d5006d4ea6
Douglas Bollella <douglas.bollella@live.com>
2016-03-22 01:31:48 -0300

    correcao Usuario

---
+++ CRUD_basico/MODEL/Usuario2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Vendas.MODEL
+{
+    
+        class Usuario
+    {
+        private int usuarioId = 0;
+        private String email = string.Empty;
+        private String senha = string.Empty;
+        private Permissao permissao;
+        private int i = 0;
+
+        public Usuario(String email, String senha)
+        {
+            this.email = email;
+            this.senha = senha;
+        }
+
+        public String Email { get; set; }
+        public String Senha { get; set; }
+
+
+    }
+}
